eaf110d5b6270f9b52f229a7c332721386d8fbf8
Zander Wepener <Zanderwepener@gmail.com>
2025-09-05 23:10:13 +0200

    Add files via upload

--- Habit-Tracker/function.js
+++ Habit-Tracker/function.js
@@ -3,19 +3,66 @@ var AddListPage = document.getElementById('Habit-Input');
 var ConfirmHabitBtn = document.getElementById('ConfirnBtn');
 var InputHabit = document.getElementById('Input');
 var ListOfHabits = document.getElementById('List');
+var ProgressBar = document.getElementById('Progresssss');
+var ProgressText = document.getElementById('Progress-Text');
 AddHabitBtn.addEventListener("click", function () {
     AddListPage.classList.remove("Hidden");
     AddListPage.classList.add("Show");
     InputHabit.focus();
 });
+var Counter = 0;
+var ItemDone = 0;
+var TotalProgress = 0;
 ConfirmHabitBtn.addEventListener("click", function () {
     var text = InputHabit.value.trim();
-    if (text === "") {
+    if (text === "")
         return;
-    }
+    Counter++;
+    TotalProgress++;
     var li = document.createElement('li');
-    li.textContent = text;
+    li.innerHTML = "\n        <div id=\"Task-Container\">\n            Task ".concat(Counter, " : ").concat(text, "  \n            <div> <button class=\"remove-btn Uncliked\">Complete Btn</button>\n            <button Class=\"RemoveBtn2\">Remove Task Btn</button></div>\n           \n        </div>\n    ");
+    var Clicked = false;
+    var removeBtn = li.querySelector(".remove-btn");
+    removeBtn.addEventListener("click", function () {
+        if (!Clicked) {
+            removeBtn.classList.remove("Uncliked");
+            removeBtn.classList.add("Clicked");
+            ItemDone++;
+        }
+        else {
+            removeBtn.classList.remove("Clicked");
+            removeBtn.classList.add("Uncliked");
+            ItemDone--;
+        }
+        // update progress bar
+        var ProgressMetric = (ItemDone / TotalProgress) * 100;
+        ProgressBar.style.width = ProgressMetric + "%";
+        ProgressText.innerHTML = "".concat(ProgressMetric.toPrecision(4), " % Done");
+        if (ProgressMetric < 0) {
+            ProgressText.innerHTML = "Calculation error";
+        }
+        else if (ProgressMetric > 100) {
+            ProgressText.innerHTML = "Calulcation error";
+        }
+        Clicked = !Clicked;
+    });
+    var Clicked2 = false;
+    var DeleteTaskBtn = li.querySelector(".RemoveBtn2");
+    DeleteTaskBtn.addEventListener("click", function () {
+        if (!Clicked2) {
+            li.remove();
+            ItemDone--;
+            TotalProgress--;
+            if (TotalProgress < 0) {
+                TotalProgress = 0;
+            }
+            if (ItemDone < 0) {
+                ItemDone = 0;
+            }
+        }
+    });
     ListOfHabits.appendChild(li);
     AddListPage.classList.add("Hidden");
     AddListPage.classList.remove("Show");
+    InputHabit.value = "";
 });
--- Habit-Tracker/function.ts
+++ Habit-Tracker/function.ts
@@ -3,28 +3,87 @@ const AddListPage = document.getElementById('Habit-Input') as HTMLDivElement;
 const ConfirmHabitBtn = document.getElementById('ConfirnBtn') as HTMLButtonElement;
 const InputHabit = document.getElementById('Input') as HTMLInputElement;
 const ListOfHabits = document.getElementById('List') as HTMLUListElement;
+const ProgressBar = document.getElementById('Progresssss') as HTMLDivElement;
+const ProgressText = document.getElementById('Progress-Text') as HTMLHeadElement;
 
 AddHabitBtn.addEventListener("click", () => {
     AddListPage.classList.remove("Hidden");
     AddListPage.classList.add("Show");
-
-   InputHabit.focus();
+    InputHabit.focus();
 });
 
-ConfirmHabitBtn.addEventListener("click", ()=>{
-   const text = InputHabit.value.trim();
+let Counter = 0;
+let ItemDone = 0;
+let TotalProgress = 0;
 
-   if (text === ""){
-    return;
-   }
+ConfirmHabitBtn.addEventListener("click", () => {
+    const text = InputHabit.value.trim();
+    if (text === "") return;
 
-   const li = document.createElement('li');
-   li.textContent = text;
+    Counter++;
+    TotalProgress++;
 
-   ListOfHabits.appendChild(li);
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <div id="Task-Container">
+            Task ${Counter} : ${text}  
+            <div> <button class="remove-btn Uncliked">Complete Btn</button>
+            <button Class="RemoveBtn2">Remove Task Btn</button></div>
+           
+        </div>
+    `;
+    
+    let Clicked = false;
+    const removeBtn = li.querySelector(".remove-btn") as HTMLButtonElement;
+
+    removeBtn.addEventListener("click", () => {
+        if (!Clicked) {
+            removeBtn.classList.remove("Uncliked");
+            removeBtn.classList.add("Clicked");
+            ItemDone++;
+        } else {
+            removeBtn.classList.remove("Clicked");
+            removeBtn.classList.add("Uncliked");
+            ItemDone--;
+        }
+
+        // update progress bar
+        const ProgressMetric = (ItemDone / TotalProgress) * 100;
+        ProgressBar.style.width = ProgressMetric + "%";
+        ProgressText.innerHTML = `${ProgressMetric.toPrecision(4)} % Done`
+        if (ProgressMetric < 0){
+            ProgressText.innerHTML = `Calculation error`;
+        } else if (ProgressMetric > 100){
+            ProgressText.innerHTML = `Calulcation error`;
+        }
+        Clicked = !Clicked;
+    });
+
+    let Clicked2 = false;
+    const DeleteTaskBtn = li.querySelector(".RemoveBtn2") as HTMLButtonElement;
+
+
+
+    DeleteTaskBtn.addEventListener("click",()=>{
+        if(!Clicked2){
+            li.remove();
+            ItemDone--;
+            TotalProgress--;
+            if (TotalProgress < 0){
+                
+                TotalProgress = 0;
+            }
+
+            if (ItemDone < 0){
+                ItemDone = 0;
+            }
+        }
+    })
+
+
+    ListOfHabits.appendChild(li);
 
     AddListPage.classList.add("Hidden");
     AddListPage.classList.remove("Show");
-    
-
-})
+    InputHabit.value = "";
+});
